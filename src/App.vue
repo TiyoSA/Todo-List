98ce34e7add0c1a31018d4fb14418acbbf8290ff
TiyoSA <tiyosapriansya88@gmail.com>
2025-05-05 20:27:16 +0700

    menambahkan fitur todo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="app">
     <h1>To-Do List</h1>
+
+    <form @submit.prevent="addTodo" class="input-section">
+      <input v-model="newTodo" placeholder="Tambah kegiatan..." />
+      <button type="submit">Tambah</button>
+    </form>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+const todos = ref([])
+
+function addTodo() {
+  if (newTodo.value.trim() !== '') {
+    todos.value.push({ text: newTodo.value, done: false })
+    newTodo.value = ''
+  }
+}
 </script>
